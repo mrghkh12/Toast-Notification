76617a0210b96291fa89e89485df9a0d9b9166c5
mohammad reza <mrghkh12@gmail.com>
2024-07-17 15:11:07 +0330

    feat(script.js): add icon in notif message

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,9 @@ const $ = document
 
 const notifBox = $.querySelector('#notifBox')
 
-let successMsg = 'Successfully submitted'
-let errorMsg = 'Please fix the error!'
-let invalidMsg = 'Invalid input, check again'
+let successMsg = '<i class="fa-solid fa-circle-check"></i>Successfully submitted'
+let errorMsg = '<i class="fa-solid fa-circle-xmark"></i>Please fix the error!'
+let invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i>Invalid input, check again'
 
 function showNotif(msg){
     console.log(msg);
