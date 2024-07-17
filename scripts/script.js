bb80cc2fc8138553ad88f6092b3f88903aa373fc
mohammad reza <mrghkh12@gmail.com>
2024-07-17 15:53:04 +0330

    style: clean codes

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,22 @@
-const $ = document
+const $ = document;
 
-const notifBox = $.querySelector('#notifBox')
+const notifBox = $.querySelector("#notifBox");
 
-let successMsg = '<i class="fa-solid fa-circle-check"></i>Successfully submitted'
-let errorMsg = '<i class="fa-solid fa-circle-xmark"></i>Please fix the error!'
-let invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i>Invalid input, check again'
+let successMsg =
+  '<i class="fa-solid fa-circle-check"></i>Successfully submitted';
+let errorMsg = '<i class="fa-solid fa-circle-xmark"></i>Please fix the error!';
+let invalidMsg =
+  '<i class="fa-solid fa-circle-exclamation"></i>Invalid input, check again';
 
-function showNotif(msg){
-    let notifElem = $.createElement('div');
-    notifElem.classList.add('toastNotif')
-    notifElem.innerHTML = msg
+function showNotif(msg) {
+  let notifElem = $.createElement("div");
+  notifElem.classList.add("toastNotif");
+  notifElem.innerHTML = msg;
 
-    msg.includes('error') && notifElem.classList.add('error')
-    msg.includes('Invalid') && notifElem.classList.add('invalid')
+  msg.includes("error") && notifElem.classList.add("error");
+  msg.includes("Invalid") && notifElem.classList.add("invalid");
 
-    notifBox.appendChild(notifElem)
+  notifBox.appendChild(notifElem);
 
-
-    setTimeout(()=>notifElem.remove(), 5000)
+  setTimeout(() => notifElem.remove(), 5000);
 }
